b87275310e3acc576202b86d12a0f85b4e0d3b51
Juan Santa Cruz <jhsantacruz@icloud.com>
2014-10-19 21:54:50 -0700

    Added products to the contorller

--- app.js
+++ app.js
@@ -1,1 +1,45 @@
-var app = angular.module('store', []);
+(function() {
+	var app = angular.module('store', []);
+
+	app.controller('StoreController', function(){
+		this.products = gems;
+	});
+
+	var gems = [
+		{
+			name: 'Test',
+			price: 2.34,
+			description: 'klahsdjla alkjdk aslkfjl sdflsdvl.',
+			canPurchase:  true,
+			soldOut: true
+		},
+		{
+			name: 'Test',
+			price: 2.34,
+			description: 'klahsdjla alkjdk aslkfjl sdflsdvl.',
+			canPurchase:  false,
+			soldOut: true
+		},
+		{
+			name: 'Test',
+			price: 2.34,
+			description: 'klahsdjla alkjdk aslkfjl sdflsdvl.',
+			canPurchase:  true,
+			soldOut: true
+		},
+		{
+			name: 'Test',
+			price: 2.34,
+			description: 'klahsdjla alkjdk aslkfjl sdflsdvl.',
+			canPurchase:  false,
+			soldOut: true
+		},
+		{
+			name: 'Test',
+			price: 2.34,
+			description: 'klahsdjla alkjdk aslkfjl sdflsdvl.',
+			canPurchase:  true,
+			soldOut: true
+		}
+	];
+})();
